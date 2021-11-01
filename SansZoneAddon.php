3431fe945db6adfe285999b191167b18450be5f5
Edo <75941214+EdoGaming28@users.noreply.github.com>
2021-11-01 09:57:59 +0700

    Create SansZoneAddon.php

---
+++ SansZoneAddon.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @name SansZoneAddon
+ * @version 1.0.0
+ * @main   JackMD\ScoreHud\Addons\SansZoneAddon
+ * @depend SansZone
+ */
+namespace JackMD\ScoreHud\Addons
+{
+
+	use JackMD\ScoreHud\addon\AddonBase;
+	use pocketmine\Player;
+	use SansZone\EdoGaming\Main;
+
+	class SansZoneAddon extends AddonBase{
+
+		/** @var SansZone */
+		private $land;
+
+		public function onEnable(): void{
+			$this->land = $this->getServer()->getPluginManager()->getPlugin("SansZone");
+		}
+
+		/**
+		 * @param Player $player
+		 * @return array
+		 */
+		public function getProcessedTags(Player $player): array{
+			return [
+				"{zona}"   => $this->land->onCheckLand($player)
+			];
+		}
+	}
+}
